52d2b3c4bf806d503ae6bf54557b7f7ffd2ef9a1
muril-o <murilomelhem@usp.br>
2020-11-14 13:02:09 -0300

    Changed url to github repo url

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
 from setuptools import setup
 
 setup(
-    # Needed to silence warnings (and to be a worthwhile package)
     name='spectrabuster',
-    url='https://github.com/jladan/package_demo',
+    url='https://github.com/eduardosprp/spectrabuster',
     author='Eduardo Lopes Dias',
     author_email='eduardotogpi@usp.br',
-    # Needed to actually package something
     packages=['spectrabuster'],
-    # Needed for dependencies
     install_requires=['numpy', 'seabreeze'],
-    # *strongly* suggested for sharing
     version='0.1',
-    # The license can be anything you like
     license='MIT',
     description='Tools for simplifying the procecssing and storing of spectrums acquired using pyseabreeze',
     # We will also need a readme eventually (there will be a warning)
